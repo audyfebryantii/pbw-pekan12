f7768ff96c245876e2bc605538469a8ee6da3dc7
audyfebryanti <febryantia21d@student.unhas.ac.id>
2023-11-09 15:13:27 +0800

    style: memberi styles pada page utama

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,25 @@
+'use client'
+
 import Image from "next/image"
+import './globals.css'
+
 export default function Home() {
+
+  function clickHandler(){
+    alert('HAI!')
+  }
+
   return (
-    <div>
+    <div className="body">
       <div className="banner-container">
       {/* Kartunya */}
         <div className="header-banner-wrapper"> 
           <div className="profile-header-banner">
           <Image
             src="/assets/profil.png"
-            width={500}
-            height={500}
             alt="Picture of the author"
+            fill
+            objectFit="contain"
           />
           </div>   
           <div className="content-bio-header-banner">
@@ -28,9 +37,11 @@ export default function Home() {
           </div>     
         </div>
         <div className="cta-banner-wrapper">
-          <button>
-            Halo!
-          </button>
+          <a href=''>
+            <div className='cta-button' onClick={clickHandler}>
+              <p>Halo!</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
