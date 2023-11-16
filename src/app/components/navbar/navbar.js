68cf2bbea76511ab63078887b27939c232e15ac8
audyfebryanti <febryantia21d@student.unhas.ac.id>
2023-11-16 18:02:34 +0800

    style: update styles in all CSS files

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -1,7 +1,6 @@
 'use client'
 
 import "@app/components/navbar/navbar.css"
-import Diary from "@app/diary/page";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -10,11 +9,13 @@ const Navbar = () => {
   const pathname = usePathname();
   return (
     <div className="navbar-container">
-      <Link className={`navbar-text ${pathname === '/' ? 'active' : 'default'}`} href="/">
+      <Link className={`navbar-link ${pathname === '/' ? 'active' : 'default'}`} href="/">
         Home
       </Link>
+
       <p>|</p>
-      <Link className={`navbar-text ${pathname === '/diary' ? 'active' : 'default'}`} href={"/diary"}>
+      
+      <Link className={`navbar-link ${pathname === '/diary' ? 'active' : 'default'}`} href={"/diary"}>
         Fetch
       </Link>
     </div>
--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -1,69 +1,59 @@
-'use client'
+"use client";
 
-import '@styles/diary.css'
-import { useEffect, useState} from 'react'
-import axios from 'axios'
+import "@styles/diary.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Diary() {
+  const [diary, setDiary] = useState([]);
+  const [isi_diary, setIsiDiary] = useState([]);
 
-    const [diary, setDiary] = useState([])
-    const [isi_diary, setIsiDiary] = useState([])
-    const[isAda, setAda] = useState(false)
-    
-    const endpointAPI = 'https://6555c14e84b36e3a431e405e.mockapi.io/diaryku'
-    // async function getAPI () {
-    //     try{
-    //         const res = await fetch(endpointAPI)
-    //         const diary = await res.json()
-    //         console.log(diary)
-    //     }
-    //     catch(error) {
-    //         console.log("error fetching data: ${error}")
-    //     }
-    // }
+  const endpointAPI = "https://6555c14e84b36e3a431e405e.mockapi.io/diaryku";
+  // async function getAPI () {
+  //     try{
+  //         const res = await fetch(endpointAPI)
+  //         const diary = await res.json()
+  //         console.log(diary)
+  //     }
+  //     catch(error) {
+  //         console.log("error fetching data: ${error}")
+  //     }
+  // }
 
-    async function getAPI () {
-        try{
-            const res = await axios.get(endpointAPI)
-            const res_isi_data = res.data
-            const diary = res_isi_data.map((dataDiary)=>(dataDiary.name))
-            setDiary(diary)
-            const isi_diary = res_isi_data.map((dataDiary)=>(dataDiary.isi_diary))
-            setIsiDiary(isi_diary)
-            // console.log(diary)
-            // console.log(isi_diary)
-            console.log(diary, isi_diary)
-        }
-        catch(error) {
-            console.log("error fetching data: ${error}")
-        }
-    }
-    
-    useEffect(() => {
-        getAPI();
-    }, [])
+  async function getAPI() {
+    const res = await axios.get(endpointAPI);
+    const res_isi_data = res.data;
 
+    const diary = res_isi_data.map((item) => item.name);
+    setDiary(diary);
 
-    console.log(isAda)
+    const isi_diary = res_isi_data.map((item) => item.isi_diary);
+    setIsiDiary(isi_diary);
 
-    return(
-        <div>
-            {diary.length > 0 ? (
-                <ul>
-                    {diary.map((item, idx)=>(
-                        <li>
-                            <div className='diary-container'>
-                                <h1>
-                                    {diary[idx]}
-                                </h1>
-                                <p className='p-diary'>
-                                    {isi_diary[idx]}
-                                </p>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            ) : ('API not loading')}
-        </div>
-    )
+    // console.log(diary)
+    // console.log(isi_diary)
+  }
+
+  useEffect(() => {
+    getAPI();
+  }, []);
+
+  return (
+    <div>
+      {diary.length > 0 ? (
+        <ul>
+          {diary.map((item, idx) => (
+            <li>
+              <div className="diary-container">
+                <h1>{diary[idx]}</h1>
+                <p className="p-diary">{isi_diary[idx]}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        "API not loading"
+      )}
+    </div>
+  );
 }
